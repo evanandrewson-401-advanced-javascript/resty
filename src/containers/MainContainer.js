0cfb64b6dbbf927035bd0ea255331226e186aaf6
evanandrewson <evanandrewson@gmail.com>
2019-11-01 16:24:00 -0700

    working on MainContainer styling

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import goFetch from '../services/fetch';
 import Display from '../components/Display';
 import History from '../components/History';
+import styles from './MainContainer.css';
 
 export default class MainContainer extends Component {
 
@@ -40,10 +41,13 @@ export default class MainContainer extends Component {
 
   render() {
     return (
-      <>
-        {this.state.history && <History items={this.state.history} />}
+      <div className={styles.MainContainer}>
+        <div className={styles.history}>
+          <h1>History</h1>
+          {this.state.history && <History items={this.state.history} />}
+        </div>
         <form onSubmit={this.handleSubmit}>
-          <input type="string" name="url" value={this.state.url} onChange={this.handleChange}></input>
+          <input className={styles.URLinput} type="string" name="url" value={this.state.url} onChange={this.handleChange}></input>
           <section>
             <button type="button" name="get" onClick={this.updateMethod}>GET</button>
             <button type="button" name="post" onClick={this.updateMethod}>POST</button>
@@ -52,18 +56,20 @@ export default class MainContainer extends Component {
             <button type="button" name="delete" onClick={this.updateMethod}>DELETE</button>
             <button >Go!</button>
           </section>
-          <input type="string" name="jsonBody" value={this.state.jsonBody} onChange={this.handleChange}></input>
-          <section>
-            <button>Headers</button>
-            <p>Basic Authorization</p>
-            <input type="string" name="username" value={this.state.username} onChange={this.handleChange}></input>
-            <input type="string" name="password" value={this.state.password} onChange={this.handleChange}></input>
-            <p>Bearer Token</p>
-            <input type="string" name="bearerToken" value={this.state.bearerToken} onChange={this.handleChange}></input>
-          </section>
+          <div className={styles.flex}>
+            <input type="string" name="jsonBody" value={this.state.jsonBody} onChange={this.handleChange}></input>
+            <section>
+              <button>Headers</button>
+              <p>Basic Authorization</p>
+              <input type="string" name="username" value={this.state.username} onChange={this.handleChange}></input>
+              <input type="string" name="password" value={this.state.password} onChange={this.handleChange}></input>
+              <p>Bearer Token</p>
+              <input type="string" name="bearerToken" value={this.state.bearerToken} onChange={this.handleChange}></input>
+            </section>
+          </div>
           <Display data={this.state.display} />
         </form>
-      </>
+      </div>
     )
   }
 }
