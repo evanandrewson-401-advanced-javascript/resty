6efbf60a9133423c72df8850a6b7761aa191e73c
evanandrewson <evanandrewson@gmail.com>
2019-11-01 16:05:58 -0700

    added footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Footer.css';
 
 const Footer = () => {
   return (
     <>
-      <footer>&copy; Abbey &amp; Evan inc.</footer>
+      <footer className={styles.Footer}>&copy; Abbey &amp; Evan inc.</footer>
     </>
   );
 };
