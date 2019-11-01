7ada32311fe9fdeac1c4c965f4c10045141fa95a
evanandrewson <evanandrewson@gmail.com>
2019-11-01 16:01:30 -0700

    added header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Header.css';
 
 const Header = () => {
   return (
     <>
-      <header>RESTy</header>
+      <header className={styles.Header}>RESTy</header>
     </>
   );
 };
