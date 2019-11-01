365c129736a306b225d846a99ffc6a7b6df8e994
evanandrewson <evanandrewson@gmail.com>
2019-11-01 11:47:09 -0700

    made Footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>&copy; Abbey &amp; Evan inc.</footer>
+    </>
+  );
+};
+
+export default Footer;
